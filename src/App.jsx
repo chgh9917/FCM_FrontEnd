ab39d8019ea19f3fe89044678497c809563c50cf
지태 <wlxo402@naver.com>
2024-09-02 12:20:01 +0900

    Feat Page add / React Router Dom Install

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import CommunityPage from "./pages/Community/CommunityPage";
+import QuestionAndAnswerPage from "./pages/QuestionAndAnswer/QuestionAndAnswerPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/community" element={<CommunityPage />} />
+      <Route path="/question" element={<QuestionAndAnswerPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/QuestionAndAnswer/QuestionAndAnswerPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function QuestionAndAnswerPage() {
+  return <div>QuestionAndAnswerPage</div>;
+}
